63197b44fc2428f2e727cb906a196c7d789276da
kmgill <kgill@jpl.nasa.gov>
2017-01-24 17:56:51 -0800

    Hmmm. I want to allow using remote image URLs (Direct from MSL or HiRISE, etc), but cross-origin restrictions...

--- uploader.js
+++ uploader.js
@@ -10,7 +10,16 @@
 
 var Uploader = function(containerId, fileSelectId, mapSetter) {
 
+    var setImageViaUrl = function(url) {
+        var span = document.createElement('span');
+        span.innerHTML = ['<img class="tex-image-preview" src="', url,
+            '" title="Loaded Image"', '" tex-uploaded="true" tex-isset="true" />'].join('');
 
+        document.getElementById(containerId).innerHTML = "";
+        document.getElementById(containerId).insertBefore(span, null);
+
+        mapSetter(url);
+    }
 
     var handleFileSelect = function(evt) {
 
@@ -25,16 +34,7 @@ var Uploader = function(containerId, fileSelectId, mapSetter) {
             var reader = new FileReader();
             reader.onload = (function(theFile) {
                 return function(e) {
-                    console.info(e);
-
-                    var span = document.createElement('span');
-                    span.innerHTML = ['<img class="tex-image-preview" src="', e.target.result,
-                        '" title="', escape(theFile.name), '"', '" tex-uploaded="true" tex-isset="true" />'].join('');
-
-                    document.getElementById(containerId).innerHTML = "";
-                    document.getElementById(containerId).insertBefore(span, null);
-
-                    mapSetter(e.target.result);
+                    setImageViaUrl(e.target.result);
                 };
             })(f);
             reader.readAsDataURL(f);
@@ -60,7 +60,9 @@ var Uploader = function(containerId, fileSelectId, mapSetter) {
         handleFileSelect(evt);
     });
 
-    return {
 
+
+    return {
+        setImageViaUrl : setImageViaUrl
     };
 };
